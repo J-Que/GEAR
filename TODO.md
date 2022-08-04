8238d53943b526b0f24b6061c6dc546e631aa4a9
J-Que <jquevedo18@uri.edu>
2022-08-04 17:56:21 -0400

    added validation file

--- TODO.md
+++ TODO.md
@@ -1,35 +1,73 @@
 # TODO
-- [ ] create program of study
 - [ ] write thesis propsal
+- [ ] write program of study
 - [ ] create timelime
 - [ ] begin thesis outline
 - [ ] research
-    - [ ] Rl
+    - [ ] NN
+    - [ ] DQN
+    - [ ] A3C
     - [ ] GA
+    - [ ] Gene Editing
+    - [ ] RLGA
+    - [ ] Parrallelizing GA
+    - [ ] Parrallelizing NN
+
+### Program TODO
+- creater footer report
+- validate solution
+- adding directory to end of indivudal
+- create function to finds unaltered point
+- create array reverse function for two opt
+- create a del list
+
+
+### REMARKS
+- store functions frequently used together, together
+- store variables frequently used together, together
+- caching
+- lookup tables
+- see which mathematic operation is done most
+- profile code
+    - investigate clock cycles
+- order of packing
+- decrease node indices for crossover process/ in original data file
+- possibly have a config file
+- creating auxilary array in fitness function? speed wise
 
 
 ### SPEED TEST
-- population storage
-    - creating thread arrays
+- order of algorithm (have each NN in series or NN in parrallel)
+- have NN on GPU vs CPU
+- have GA on GPU vs CPU vs both
+- population abstraction
+    - multidimensional array of integers
+    - multidimensional array of node objects (if CPU/both)
+    - array of individual objects (if CPU/both)
+    - priority queue of individual objects
+- individual information
+    - have extra index for auxilary random value (arv)
+    - order or nodes, capacity, and potentially the (arv)
+- order of packing
+    - single permutation
+    - changing permutation for each operation
+    - changing permutation for each few operations
+- 
+    - creating thread arrays for cpu parralelization
 - population abstraction
     - multidimension array of integers
     - multidimension array of node objects
     - array of individual objects
     - priorty queue
-- encoding (last)
-    - single permuation for all operations
-    - one permutation for 2 and 1 operations
-    - different permuation for each operation
-- sorting
-    - exact fitness
-        - by pointers
-        - by whole object
-    - estimated fitness
-        - by pointers
-        - by whole objects
+
 
 
 ### QUALITY TEST
+- pairing
+    - pair randomly (any) and get one child
+    - pair randomly (permutation) and get one child
+    - pair randomly (any) and get two children
+    - pair randomly (permutation) and get two children
 - crossover
     - resolution
     - operators
@@ -44,22 +82,8 @@
     - operators
       - inverse
       - scrambled
-- replacement
-    - inplace of population array
-    - seperate children and parent array
 - two opt frequency
-- migration frequenecy
+- migration frequenecy (if CPU/both)
 - fitness metrics
     - euclidean
     - squared distances
-
-
-### REMARKS
-- store functions frequently used together, together
-- store variables frequently used together, together
-- use vector operations
-- caching
-- lookup tables
-- see which mathematic operation is done most
-- profile code
-    - investigate clock cycles
--- bin/main/main.py
+++ bin/main/main.py
@@ -1,16 +1,13 @@
 import os
 import sys
 import json
+import cupy as cp
 import numpy as np
+from numba import cuda
 os.chdir(os.path.dirname(__file__) + '/../../')
 sys.path.insert(0, 'bin/lib')
 import report
 
-
-# order of packing
-# inline values
-# decrease node index
-# check if nodes ar eshuffled
 
 # read in the problem file
 def read(file=None, generations=None, populationSize=None, mutationRate=None):
@@ -76,10 +73,112 @@ def populate(nodes, params, cvrp):
         # set the shuffled nodes to the indiviual, the last node is reserved for the cost
         population[m][:-1] = nodes
     
-    return population
+    # send the population to the gpu
+    return cuda.to_device(population) 
+
+
+# establish routes base of capacity, conduct two-opt the routes, and get the cost of each route
+@cuda.jit
+def fitness(M, N, population, capacity, depot):
+    threadY, threadX = cuda.grid(2)
+    strideY, strideX = cuda.gridsize(2)
+
+    # iterate through the individuals
+    for m in range(threadY, M, strideY):
+
+        #------------------------------------------------------------------------------------------
+        # determine the altered point (where a change in the genes was made)
+        altered, unaltered = population[m, N], 0
+
+        # find the first route point from the altered point
+        for n in range(altered, 0, -1):
+
+            # detemine if the node value is odd (meaning it has a depot visit before its visit)
+            if population[m, n] & 1 and True or False:
+
+                # is so the unaltered point is found
+                unaltered = n
+                break
+
+        #------------------------------------------------------------------------------------------
+        # start of a new route, the route length is 2 because it includes both depots at the end
+        routeLength, load, fitness, improved = 2, 0, 0, True
+
+        # iterate through the nodes
+        for n in range(threadX, N):
+
+            #------------------------------------------------------------------------------------------
+            # get the cost of the unaltered nodes
+            if n < unaltered:
+
+                # get the cost of the nodes
+                fitness += 
+
+                # once the cost is obtained pass on to the next nodes
+                continue
+
+            #------------------------------------------------------------------------------------------
+            # determine if the current node has a depot visit before its visit (meaning it is odd)
+            if population[m, n] & 1 and True or False:
+                
+                # if so then the depot visit is reset to none (meaning one is subtracted from the node value)
+                population[m, n] -= 1
+            
+            #------------------------------------------------------------------------------------------
+            # unpack the demand
+            demand = 
+
+            # if the load plus addidtional demand does not violate the capacity
+            if demand + load <= capacity:
+
+                # then the route length is incremented and the demand is added to the load, the distance is added to the fitness and the route continues
+                routeLength += 1
+                load += demand
+                fitness += 
+
+            #------------------------------------------------------------------------------------------
+            # else the capacity has been violated, meaning this is the end of the route
+            else:
+                
+                #an auxilary array is used to load the route
+                route = cp.zeros(routeLength)
+
+                # the nodes are loaded onto the array, including the two depots at the end
+                route[0], depot[-1] = depot, depot
+                route[1:-1] = population[m, n - routeLength + 2: n]
+                
+                # add the distances from the endpoints to the depot
+                fitness += 
+
+                # two opt is conducted on the route
+                while improved:
+                    improved = False
+
+                    # 1 is starting index because the depot is at the begining and cannot be swapped, the last node cannot be iterated because it also cannot be swapped
+                    for nodeI in range(1, routeLength):
+
+                        # starting at the current node iterate through the remaining nodes
+                        for nodeJ in range(nodeI + 1, routeLength + 1):
+
+                            # find the change in cost if the swap is performed
+                            delta = 
+
+                            # if the swap could reduce the cost, then the 2-opt swap is confirmed
+                            if delta < 0:
+                                do2opt =
+                                fitness += delta
+                                improved = True
+
+                # a new route is started
+                routeLength, load = 2, 0
+    cuda.syncthreads()
 
 
 if __name__ == '__main__':
+    # GPU grid configurations:
+    blocks = (4, 4)
+    threads = (32, 16)
+
     # print a report header
     report.header()
 
@@ -91,11 +190,10 @@ if __name__ == '__main__':
 
     # populate with individuals
     population = populate(nodes, params, cvrp)
+   
+    # get the fitness of the population
+    fitness[blocks, threads](cvrp['Capacity'], )
 
-    # adjust the population for the capacity constraints
-    
-    # get the cost of the population
-    
     # evolve the population
     for generation in range(params['generations']):
         pass
